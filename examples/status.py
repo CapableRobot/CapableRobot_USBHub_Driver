eb9e0101b104f9134cd0d06112b1a9de7a00d8b5
Chris Osterwood <osterwood@gmail.com>
2019-06-11 09:16:34 -0400

    Added script which exposed useful state and measurement

---
+++ examples/status.py
@@ -0,0 +1,28 @@
+import os, sys, inspect
+import time
+import logging
+
+lib_folder = os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0], '..')
+lib_load = os.path.realpath(os.path.abspath(lib_folder))
+
+if lib_load not in sys.path:
+    sys.path.insert(0, lib_load)
+
+FORMAT = '%(levelname)-8s %(message)s'
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger('USB Hub Status')
+logger.setLevel(logging.DEBUG)
+
+import capablerobot_usbhub 
+
+hub = capablerobot_usbhub.USBHub()
+hub.i2c.enable()
+
+
+logger.info("Port Connections : {}".format(hub.connections()))
+logger.info("Port Speeds      : {}".format(hub.speeds()))
+
+logger.info("Port Currents (mA)")
+logger.info("     Measured : " + " ".join([("%.2f" % v).rjust(7) for v in hub.currents()]))
+logger.info("     Limit    : {}".format(hub.current_limits()))
+logger.info("     Alerts   : {}".format(" ".join(hub.current_alerts())))
